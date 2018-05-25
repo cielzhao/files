03af8265f61fcab3ce05965138af4192d5e491ee
cielzhao <1425066368@qq.com>
2018-05-25 22:14:43 +0800

    add todo item

---
+++ vue/src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h3>我是子组件</h3>
+		<ol>
+			<li v-for="(item,key) in todo" :key="key">{{ item.text }}</li>
+		</ol>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'TodoItem',
+		props: ['todo']
+	}
+</script>
+
+<style>
+</style>
